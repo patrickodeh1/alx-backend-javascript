1a8e870c393945968c7154ca686bed904323b014
Patrick Odeh <vyltyan@gmail.com>
2024-10-04 13:14:19 +0100

    2-then.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
       console.log(photoResult, userResult.firstName, userResult.lastName);
       return { photoResult, userResult };
