ac9d37aa11e18f47ba3751b8208f2ff4ca5fb97d
Patrick Odeh <vyltyan@gmail.com>
2024-10-04 10:51:32 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,15 @@
 export default function handleResponseFromAPI(promise) {
   const p = new Promise((resolve, reject) => {
-    if (promise) {
+    if (promise === true) {
       return resolve({ status: 200, body: 'Success' });
-    } else {
-      return reject(new Error());
     }
+    return reject(new Error());
   });
 
   p.then(() => {
     console.log('Got a response from the API');
+  }).catch(() => {
+    console.log('Got a response from the API');
   });
+  return p;
 }
