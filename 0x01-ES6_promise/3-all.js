e4b76ca43cb3687b45ee83b41e615e98d21dabea
Patrick Odeh <vyltyan@gmail.com>
2024-10-04 12:22:55 +0100

    2-then.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,11 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
-      console.log(photoResult.body, userResult.firstName, userResult.lastName);
+      console.log(photoResult, userResult.firstName, userResult.lastName);
+      return {photoResult, userResult};
     })
     .catch(() => {
       console.log('Signup system offline');
+      return Promise.reject("Signup system offline");
     });
 }
