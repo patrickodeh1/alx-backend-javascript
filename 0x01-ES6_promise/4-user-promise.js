ae5c078f515cd747e1d862375f9236013dbdc984
Patrick Odeh <vyltyan@gmail.com>
2024-10-04 11:53:44 +0100

    2-then.js

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+export default function signUpUser(firstName, lastName) {
+    return new Promise((resolve, reject) => {
+        if (firstName ) {
+            resolve({firstName, lastName})
+        } else {
+            reject()
+        }
+    })
+}
