d9b7d09715aa2750afd6ff35cc47477ac915d305
Patrick Odeh <vyltyan@gmail.com>
2024-10-04 10:06:28 +0100

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
-      const message = { status: '200', body: 'Success' };
-      resolve(message);
+      resolve({ status: '200', body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
