0c3a8dd8cad02c0d33621e03d36c7ff0f2c94c51
Patrick Odeh <vyltyan@gmail.com>
2024-10-05 07:51:38 +0100

    2-then.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default async function handleProfileSignup(firstName, lastName, filename) {
   const user = signUpUser(firstName, lastName);
   const pic = uploadPhoto(filename);
   return Promise.allSettled([user, pic])
 
     .then((res) => res.map((r) => ({
       status: r.status,
-      value: r.status === 'fulfilled' ? r.value : r.reason,
+      value: r.status === 'fulfilled' ? r.value : String(r.reason),
     })));
 }
