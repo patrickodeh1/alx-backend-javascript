fe682663660dea90003b30b184f718c65c1fc67f
Patrick Odeh <vyltyan@gmail.com>
2024-10-05 15:20:13 +0100

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,6 +5,8 @@ export default class HolbertonCourse {
       this._name = name;
       this._length = length;
       this._students = students;
+    } else {
+      throw new TypeError("invalid type");
     }
   }
 
