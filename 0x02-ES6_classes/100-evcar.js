deed4503ed59eeb31e99150d75e84da8afefeda4
Patrick Odeh <vyltyan@gmail.com>
2024-10-06 09:44:31 +0100

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,7 +6,7 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  static cloneCar() {
-    return new Car(undefined, undefined, undefined);
+  cloneCar() {
+    return super.cloneCar();
   }
 }
