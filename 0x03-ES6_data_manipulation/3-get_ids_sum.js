dd2f59febbe3d8185ca51db02540582fe112a70c
Patrick Odeh <vyltyan@gmail.com>
2024-10-09 20:29:54 +0100

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,7 @@
 export default function getStudentIdsSum(arr) {
-
+    if (arr instanceof Array) {
+        const ids = arr.reduce((accumulator, id) => {
+            return accumulator + id.id;
+        }, 0)
+    }
 }
