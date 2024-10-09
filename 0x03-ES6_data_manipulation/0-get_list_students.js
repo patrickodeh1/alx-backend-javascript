ae2aed356ddea3505cdbba4ef465d9add865de2e
Patrick Odeh <vyltyan@gmail.com>
2024-10-09 20:46:45 +0100

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
-export default function getListStudents() {
-  return [
+export default function getListStudents() { 
+  const students = [
     { id: 1, firstName: 'Guilaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+  return students;
 }
