837ec1b8bfc7893cb25c0fd5a3c3b694db00b678
Patrick Odeh <vyltyan@gmail.com>
2024-10-13 22:05:15 +0100

    typed_arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const intArray = new Int8Array(buffer);
-  if (value < -128 || value > 127) {
+  if (position < 0 || position > length) {
     throw new RangeError('Position outside range');
   }
   intArray[position] = value;
