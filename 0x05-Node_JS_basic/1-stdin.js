684bcefdf7a2a6a0ff54ecd6dfbccc5b43ed08da
Patrick Odeh <vyltyan@gmail.com>
2024-11-26 10:29:20 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,14 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const process = require('process');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-rl.on('SIGINT', () => {
-  console.log('\nThis important software is now closing');
-  rl.close();
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
