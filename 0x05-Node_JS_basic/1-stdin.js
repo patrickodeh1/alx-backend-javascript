1d1e2f390cee3b8fae02b398c3a9ce24c333efbd
Patrick Odeh <vyltyan@gmail.com>
2024-11-26 09:42:23 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,13 @@
 const readline = require('readline');
 
-
 const r = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
 console.log('Welcome to Holberton School, what is your name?');
-r.on('line', (input) => {
-  if (input.trim()) {
-    console.log(`Your name is: ${input}`);
-  }
+r.question('', (name) => {
+  console.log(`Your name is: ${name}`);
   r.close();
 });
 
