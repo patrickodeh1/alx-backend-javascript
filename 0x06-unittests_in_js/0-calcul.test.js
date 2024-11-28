87bc4c1fba9fa3d80f5fd7021090d81421f7b4b2
Patrick Odeh <vyltyan@gmail.com>
2024-11-28 02:54:54 +0100

    tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,6 +3,15 @@ const calculateNumber = require('./0-calcul')
 
 describe('calculateNumber', function () {
   it("calculateNumber should return 5 when 2 and 2.6 are passed", function () {
-    assert.equal(calculateNumber(2, 2.6), '5');
+    assert.strictEqual(calculateNumber(2, 2.6), 5);
+  });
+  it("calculateNumber should return 5 when 2.5 and 2.4 are passed", function () {
+    assert.strictEqual(calculateNumber(2.5, 2.4), 5);
+  });
+  it("calculateNumber should return 0 when 0 and 0.3 are passed", function () {
+    assert.strictEqual(calculateNumber(0.3, 0.3), 0);
+  });
+  it("calculateNumber should return -4 when -2 and -2.4 are passed", function () {
+    assert.strictEqual(calculateNumber(-2, -2.4), -4);
   });
 });
