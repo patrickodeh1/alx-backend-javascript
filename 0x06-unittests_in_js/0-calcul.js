9b4011bdc6b2f42c7d968a13411d0498fb0b1fcc
Patrick Odeh <vyltyan@gmail.com>
2024-11-28 02:22:03 +0100

    0-calcul.test.js

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,5 @@
+export default function calculateNumber(a, b) {
+    a = Math.round(a)
+    b = Math.round(b)
+    return a + b;
+}
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,8 @@
+import { assert } from 'chai';
+import calculateNumber from './0-calcul.js'
+
+describe('', function () {
+    it("calculateNumber should round floating number and return the sum", function () {
+        assert.equal(calculateNumber(2, 2.6), '5');
+    })
+})
