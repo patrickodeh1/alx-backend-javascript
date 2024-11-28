76286074e6e5e4eb88592e4efcfaf162a946443a
Patrick Odeh <vyltyan@gmail.com>
2024-11-28 02:32:53 +0100

    tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import calculateNumber from './0-calcul.js'
 
-describe('', function () {
+describe('calculateNumber', function () {
     it("calculateNumber should round floating number and return the sum", function () {
         assert.equal(calculateNumber(2, 2.6), '5');
     })
