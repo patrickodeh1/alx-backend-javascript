8f8d81332af4c1ec2144f1fec6419a3cc18685da
Patrick Odeh <vyltyan@gmail.com>
2024-11-29 20:52:05 +0100

    8-api/api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,38 +1,26 @@
 const chai = require('chai');
-const chaiHttp = require('chai-http');
+const fetch = require('node-fetch');
 const {expect} = chai;
 const app = require('./api');
 const server = app.listen(7865);
 
-chai.use(chaiHttp);
-
 describe('Index Page', function () {
-    it('should return the correct status code', function (done) {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                done();
-            });
+    it('should return the correct status code', async function () {
+        const res = await fetch('http://localhost:7865/');
+        expect(res.status).to.equal(200);
     });
-    it('should return the correct result', function (done) {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(res.text).to.equal('Welcome to the payment system');
-                done();
-            });
+    it('should return the correct result', async function () {
+        const res = await fetch('http://localhost:7865/');
+        const text = await res.text();
+        expect(text).to.equal('Welcome to the payment system');
     });
-    it('should not have an error', function (done) {
-        chai.request(app)
-            .get('/')
-            .end((err) => {
-                expect(err).to.be.null;
-                done();
-            });
+
+    it('should not have an error', async function () {
+        const res = await fetch('http://localhost:7865/');
+        expect(res.ok).to.be.true; // Check if the response was successful
     });
 });
 
 after(function (done) {
     server.close(done);
-  });
+});
